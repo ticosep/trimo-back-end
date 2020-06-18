e2dc6b6332fb5c69182a81e46edd4328deb93e87
egistoparronchi-pushstart <egisto.parronchi@pushstart.com.br>
2020-06-17 22:36:25 -0300

    Add support for new validation links

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -5,9 +5,37 @@ const { transporter } = require("../services/email");
 
 const router = express.Router();
 
-// Set the databse conection to use the user DB
-database.query("USE core", (err, result) => {
-  if (err) throw err;
+const sendValidationEmail = ({ id, email }) => {
+  const token = jwt.sign({ id }, jwtOptions.secretOrKey);
+
+  const url = `http://localhost:3030/confirmation/${token}`;
+
+  transporter.sendMail({
+    to: email,
+    subject: "Trimo - Confirmação de email",
+    html: `Por favor clique no link para desbloquear sua conta e acessar o trimo: <a href="${url}">${url}</a>`,
+  });
+};
+
+router.post("/validate", async (request, res) => {
+  const { id, email } = request.body;
+
+  const isValid = !!id && !!email;
+
+  if (!isValid) {
+    res.sendStatus(400);
+    return;
+  }
+
+  try {
+    sendValidationEmail({ id, email });
+
+    res.sendStatus(200);
+    return;
+  } catch (error) {
+    res.sendStatus(400);
+    return;
+  }
 });
 
 // Try to create a new user in the database, if email allreay reponse a error code
@@ -22,19 +50,13 @@ router.post("/", async (request, res) => {
   }
 
   try {
+    await query("USE core");
+
     const { insertId } = await query(
       `INSERT INTO users (name, surname, email, password) VALUES ('${name}', '${surname}', '${email}', '${password}')`
     );
 
-    const token = jwt.sign({ id: insertId }, jwtOptions.secretOrKey);
-
-    const url = `http://localhost:3030/confirmation/${token}`;
-
-    transporter.sendMail({
-      to: email,
-      subject: "Trimo - Confirmação de email",
-      html: `Por favor clique no link para desbloquear sua conta e acessar o trimo: <a href="${url}">${url}</a>`,
-    });
+    sendValidationEmail({ id: insertId, email });
 
     res.sendStatus(200);
     return;
