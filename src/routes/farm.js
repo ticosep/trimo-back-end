65cab32507a69ca9217ba5d8c8a795e9cc857359
egistoparronchi-pushstart <egisto.parronchi@pushstart.com.br>
2020-06-23 16:28:02 -0300

    Add get for farms register in the user

--- src/routes/farm.js
+++ src/routes/farm.js
@@ -7,6 +7,7 @@ const {
   getMapFeatures,
   setMapFeatures,
 } = require("../models/farm");
+const { query } = require("../services/database");
 
 // Create a new farm
 router.post(
@@ -61,23 +62,36 @@ router.put(
 );
 
 router.get(
-  "/map/:farm_id",
+  "/user",
   passport.authenticate(STRATEGYS.USER, { session: false }),
   async (request, res) => {
-    const farm_id = request.params.farm_id;
-    const { farms } = request.user;
-
-    const hasAccessToFarm = farms.some((farm) => +farm === +farm_id);
-
-    const isValid = !!farm_id && hasAccessToFarm;
-
-    if (!isValid) {
-      res.sendStatus(400);
-      return;
-    }
+    const { id, farms, is_worker } = request.user;
 
     try {
-      getMapFeatures({ farm_id }, res);
+      await query("USE core");
+
+      const [farm] = farms;
+
+      if (is_worker) {
+        const info = await query(`
+        SELECT * FROM farms 
+        WHERE id = ${farm}        
+        `);
+
+        res.status(200).json({ farms: info });
+      }
+
+      if (!is_worker) {
+        const farms = await query(`
+        SELECT * FROM farms AS A
+        INNER JOIN farm_users AS B
+        ON A.id = B.farm
+        WHERE B.user_id = ${id}
+        `);
+
+        res.status(200).json({ farms });
+      }
+
       return;
     } catch (error) {
       res.status(404).json({ error });
